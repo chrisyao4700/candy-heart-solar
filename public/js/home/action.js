e7963e6d0eb1cd9e91d9bcc2d2663e4a2617bae6
Yuan Yao <yuanyao@Yuans-MBP-7.local>
2018-04-19 13:47:28 -0700

    SAVE FOR CHANGE

--- public/js/home/action.js
+++ public/js/home/action.js
@@ -2,7 +2,7 @@ function init_map() {
     let mapOptions = {
         center: new google.maps.LatLng(34.0900, -117.8903),
         zoom: 11,
-        mapTypeId: google.maps.MapTypeId.TERRAIN
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     let map = new google.maps.Map(document.getElementById("map"), mapOptions);
     let marker = new google.maps.Marker({
--- routes/index.router.js
+++ routes/index.router.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var menuConn = require('../modules/menu/menu.conn');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('home/page_root.ejs', {title: 'Tassie Cake'});
+    res.render('home/page_root.ejs', {
+        title: 'Tassie Cake',
+        menuItems: menuConn.getMenuList()
+    });
 });
 
 module.exports = router;
