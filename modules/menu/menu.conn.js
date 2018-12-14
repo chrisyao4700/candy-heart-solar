6b02b4e9037062d2372acf90c6aa5193ffe9bdd4
leonwqiao <leonwqiao@gmail.com>
2018-12-13 17:05:22 -0800

    price update

--- modules/menu/menu.conn.js
+++ modules/menu/menu.conn.js
@@ -4,7 +4,7 @@ const getMenuList = () => {
             name: '草莓千层',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-0.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-0.jpg',
-            price: '6\'\' $39 | 8\'\' $60 | 10\'\' $80',
+            price: '6\'\' $39 | 9\'\' $69',
             des: '<i class="fa fa-star"></i> 使用草莓果酱做成的酸甜口草莓可丽饼<br>' +
             '<i class="fa fa-star"></i> 鲜奶油+酸奶，再搭配草莓果酱做成酸甜平衡的草莓奶油<br>' +
             '<i class="fa fa-star"></i> 加入新鲜草莓果肉增加口感<br>'
@@ -13,7 +13,7 @@ const getMenuList = () => {
             name: '红豆抹茶千层',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-1.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-1.jpg',
-            price: '6\'\' $42 | 8\'\' $62 | 10\'\' $82',
+            price: '6\'\' $42 | 9\'\' $72',
             des: '<i class="fa fa-star"></i> 选用日式特级抹茶粉，口味纯正<br>' +
             '<i class="fa fa-star"></i> 抹茶粉从运输到使用持续低温保鲜，最大程度保证抹茶的新鲜度<br>' +
             '<i class="fa fa-star"></i> 再加入自制蜜豆综合口感<br>'
@@ -22,7 +22,7 @@ const getMenuList = () => {
             name: '榴莲千层',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-2.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-2.jpg',
-            price: '6\'\' $45 | 8\'\' $72 | 10\'\' $98',
+            price: '6\'\' $45 | 9\'\' $85',
             des: '<i class="fa fa-star"></i> 选用猫山王榴莲，口味纯正<br>' +
             '<i class="fa fa-star"></i> 搭配烤杏仁片，丰富口感<br>'
         },
@@ -30,7 +30,7 @@ const getMenuList = () => {
             name: '椰子千层',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-3.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-3.jpg',
-            price: '6\'\' $39 | 8\'\' $59 | 10\'\'  $79',
+            price: '6\'\' $39 | 9\'\'  $69',
             des: '<i class="fa fa-star"></i> 椰子口味可丽饼+清爽的椰子奶油<br>' +
             '<i class="fa fa-star"></i> 再搭配椰蓉增加层次感<br>'
         },
@@ -38,7 +38,7 @@ const getMenuList = () => {
             name: '提拉米苏千层',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-4.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-4.jpg',
-            price: '6\'\' $42 | 8\'\' $62 | 10\'\' $82',
+            price: '6\'\' $42 | 9\'\' $72',
             des: '<i class="fa fa-star"></i> 提拉米苏千层，使用提拉米苏所必备的几样食材而做成<br>' +
             '<i class="fa fa-star"></i> 饼皮是根据传统提拉米苏里有的意式浓缩咖啡做成的可丽饼<br>' +
             '<i class="fa fa-star"></i> 马斯卡彭芝士+新鲜蛋黄做成冰淇淋口感的内馅<br>'
@@ -47,7 +47,7 @@ const getMenuList = () => {
             name: '巧克力千层',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-5.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-5.jpg',
-            price: '6\'\' $42 | 8\'\' $62 | 10\'\' $82',
+            price: '6\'\' $42 | 9\'\' $72',
             des: '<i class="fa fa-star"></i> 选用100%可可脂巧克力制作巧克力可丽饼<br>' +
             '<i class="fa fa-star"></i> 70%黑巧克力+有机鲜奶油做成内馅<br>' +
             '<i class="fa fa-star"></i> 使用12种巧克力做成，醇而不腻<br>'
@@ -89,7 +89,7 @@ const getEnMenuList = () => {
             name: 'Strawberry Mille Crepes',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-0.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-0.jpg',
-            price: '6\'\' $39 | 8\'\' $60 | 10\'\' $80',
+            price: '6\'\' $39 | 9\'\' $69',
             des: '<i class="fa fa-star"></i> The crepes are made with natural strawberry jam.<br>' +
             '<i class="fa fa-star"></i> The strawberry cream is made with yogurt and strawberry jam. <br>' +
             '<i class="fa fa-star"></i> Adding some fresh strawberries for taste.<br>'
@@ -98,7 +98,7 @@ const getEnMenuList = () => {
             name: 'Matcha Red Bean Mille Crepes',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-1.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-1.jpg',
-            price: '6\'\' $42 | 8\'\' $62 | 10\'\' $82',
+            price: '6\'\' $42 | 9\'\' $72',
             des: '<i class="fa fa-star"></i> We use pure taste Japanese Matcha powder.<br>' +
             '<i class="fa fa-star"></i> The Matcha powder is kept in low temperature all-time to ensure its freshness.<br>' +
             '<i class="fa fa-star"></i> Adding homemade honey beans to improve the taste.<br>'
@@ -107,7 +107,7 @@ const getEnMenuList = () => {
             name: 'Durian Mille Crepes',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-2.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-2.jpg',
-            price: '6\'\' $45 | 8\'\' $72 | 10\'\' $98',
+            price: '6\'\' $45 | 9\'\' $85',
             des: '<i class="fa fa-star"></i> The cream is made with values durian.<br>' +
             '<i class="fa fa-star"></i> Adding baked almond slices for more texture.<br>'
         },
@@ -115,7 +115,7 @@ const getEnMenuList = () => {
             name: 'Coconut Mille Crepes',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-3.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-3.jpg',
-            price: '6\'\' $39 | 8\'\' $59 | 10\'\'  $79',
+            price: '6\'\' $39 | 9\'\'  $69',
             des: '<i class="fa fa-star"></i> Soft coconut crepes + fresh coconut cream.<br>' +
             '<i class="fa fa-star"></i> Topped with shredded coconut stuffing<br>'
         },
@@ -123,7 +123,7 @@ const getEnMenuList = () => {
             name: 'Tiramisu Mille Crepes',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-4.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-4.jpg',
-            price: '6\'\' $42 | 8\'\' $62 | 10\'\' $82',
+            price: '6\'\' $42 | 9\'\' $72',
             des: '<i class="fa fa-star"></i> Made of several ingredients necessary for Tiramisu.<br>' +
             '<i class="fa fa-star"></i> The crepes are made with traditional Italian espresso.<br>' +
             '<i class="fa fa-star"></i> The cream is made with cheese and fresh egg yolk. <br>'
@@ -132,7 +132,7 @@ const getEnMenuList = () => {
             name: 'Chocolate Mille Crepes',
             iconPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-icon/menu-icon-5.png',
             imgPath: 'https://s3-us-west-1.amazonaws.com/candy-heart-storage/menu-item/menu-item-5.jpg',
-            price: '6\'\' $42 | 8\'\' $62 | 10\'\' $82',
+            price: '6\'\' $42 | 9\'\' $72',
             des: '<i class="fa fa-star"></i> The crepes are made with 100% cocoa chocolate.<br>' +
             '<i class="fa fa-star"></i> The cream is made with 70% dark chocolate and organic whipped cream.<br>' +
             '<i class="fa fa-star"></i> This cake contains 12 different kinds of chocolate. <br>'
